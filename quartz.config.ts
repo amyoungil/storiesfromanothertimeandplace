0668a5c7c0429fbad3af4b27e001f62e87724c1f
amyoungil <nutellacrepe@gmail.com>
2024-03-23 14:46:16 -0500

    Quartz sync: Mar 23, 2024, 2:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Stories from another time and place",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://amyoungil.github.io/storiesfromanothertimeandplace/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
